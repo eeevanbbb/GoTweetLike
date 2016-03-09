006cfe4ad0bb99999e796d1756fc3b383f0414e2
Evan Bernstein <eeevanbbb@me.com>
2016-03-08 19:28:05 -0600

    altered request parsing

--- RequestParser.py
+++ RequestParser.py
@@ -6,11 +6,11 @@ def get_tweet_type(tweet):
 	if len(words) < 2:
 		return "Invalid"
 	elif words[0].lower() == "@gotweetlike" and words[1][0] == "@":
-		if len(words) == 2:
-			return "Standard"
-		elif len(words) == 3 and words[2].lower() == "stats":
+		if len(words) == 3 and words[2].lower() == "stats":
 			return "Stats"
 		elif len(words) == 4 and words[2].lower() == "about":
 			return "Topic: " + words[3]
+		else:
+			return "Standard"
 	
 	return "Invalid"
