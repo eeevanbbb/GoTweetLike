045f4ed40653758d1d3363cebcb5bae89a0b85f5
Evan Bernstein <eeevanbbb@me.com>
2016-04-18 15:18:24 -0500

    more tolerant request parsing

--- RequestParser.py
+++ RequestParser.py
@@ -6,13 +6,13 @@ def get_tweet_type(tweet):
 	if len(words) < 2:
 		return "Invalid"
 	elif words[0].lower() == "@gotweetlike" and words[1][0] == "@":
-		if len(words) == 3 and words[2].lower() == "stats":
+		if len(words) >= 3 and words[2].lower() == "stats":
 			return "Stats"
-		elif len(words) == 3 and words[2].lower() == "update":
+		elif len(words) >= 3 and words[2].lower() == "update":
 			return "Update"
 		elif len(words) == 4 and words[2].lower() == "about":
 			return "Topic: " + words[3]
-		elif len(words) == 4 and words[2].lower() == "advanced" and words[3].lower() == "stats":
+		elif len(words) >= 4 and words[2].lower() == "advanced" and words[3].lower() == "stats":
 			return "AdvancedStats"
 		else:
 			return "Standard"
