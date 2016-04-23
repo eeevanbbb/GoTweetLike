f89eb3975f456e542a15aef068ad13ea2884a665
Evan Bernstein <eeevanbbb@me.com>
2016-04-23 17:25:06 -0500

    more lenient request handling

--- RequestParser.py
+++ RequestParser.py
@@ -6,6 +6,8 @@ def get_tweet_type(tweet):
 	if len(words) < 2:
 		return "Invalid"
 	elif words[0].lower() == "@gotweetlike" and words[1][0] == "@":
+		scannableWords = words[2:]
+		scannableString = " ".join(scannableWords).lower()
 		if len(words) >= 3 and words[2].lower() == "stats":
 			return "Stats"
 		elif len(words) >= 3 and words[2].lower() == "update":
@@ -16,6 +18,16 @@ def get_tweet_type(tweet):
 			return "AdvancedStats"
 		elif len(words) >= 4 and words[2].lower() == "longest" and (words[3].lower() == "word" or words[3].lower() == "words"):
 			return "LongestWord"
+		#More lenient scanning
+		elif "longest word" in scannableString or "longest words" in scannableString:
+			return "LongestWord"
+		elif "advanced stats" in scannableString or "more stats" in scannableString:
+			return "AdvancedStats"
+		elif "stats" in scannableString:
+			return "Stats"
+		elif "update" in scannableString:
+			return "Update"
+		#Default to standard
 		else:
 			return "Standard"
 	
