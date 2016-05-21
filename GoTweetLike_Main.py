2b954bc8dce3f75fede467ecf8e6f99dce44548c
Evan Bernstein <eeevanbbb@me.com>
2016-05-20 19:49:21 -0500

    seriously atom needs to fix its python invisibles

--- GoTweetLike_Main.py
+++ GoTweetLike_Main.py
@@ -113,14 +113,14 @@ class MyStreamListener(tweepy.StreamListener):
 							new_tweet = generate_advanced_stats_for_tweets(tweets)
 						elif tweet_type == "LongestWord":
 							new_tweet = get_longest_word_tweet_for_tweets(tweets)
-                        elif tweet_type == "Rant":
-                            new_tweet = generate_rant(600,1000,tweets) #min = 600, max = 1000 chars
+						elif tweet_type == "Rant":
+							new_tweet = generate_rant(600,1000,tweets)
 
 
-                        address = "@%s @%s: " % (tweeter_screen_name, username_to_tweet_like)
-                        full_tweet = address + new_tweet
+						address = "@%s @%s: " % (tweeter_screen_name, username_to_tweet_like)
+						full_tweet = address + new_tweet
 
-                        outgoing_tweets = []
+						outgoing_tweets = []
 
 						#Okay, this will take some explaining
 						#If the full tweet is <= 140 chars, perfect, we're done
@@ -130,55 +130,55 @@ class MyStreamListener(tweepy.StreamListener):
 							#This means we have to calculate the overhead beforehand
 							#And divide accordingly, until there are no more chars left
 
-                        if len(full_tweet) <= 140:
-                            outgoing_tweets.append(full_tweet)
-                        else:
-                            addressLength = len(address)
-                            countLength = len("(?/?) ") #NOTE: Only supports up to 9 tweets
-                            overheadLength = addressLength + countLength
-                            maxTextLength = 140 - overheadLength
-                            tweetNumber = 0
-                            totalTweets = int(math.ceil(float(len(new_tweet)) / float(maxTextLength)))
-                            while len(new_tweet) > 0:
-                                tweetNumber += 1
-                                partialTweet = address + "(%d/%d) " % (tweetNumber,totalTweets) + new_tweet[:maxTextLength]
-                                outgoing_tweets.append(partialTweet)
-                                new_tweet = new_tweet[maxTextLength:]
+						if len(full_tweet) <= 140:
+							outgoing_tweets.append(full_tweet)
+						else:
+							addressLength = len(address)
+							countLength = len("(?/?) ") #NOTE: Only supports up to 9 tweets
+							overheadLength = addressLength + countLength
+							maxTextLength = 140 - overheadLength
+							tweetNumber = 0
+							totalTweets = int(math.ceil(float(len(new_tweet)) / float(maxTextLength)))
+							while len(new_tweet) > 0:
+								tweetNumber += 1
+								partialTweet = address + "(%d/%d) " % (tweetNumber,totalTweets) + new_tweet[:maxTextLength]
+								outgoing_tweets.append(partialTweet)
+								new_tweet = new_tweet[maxTextLength:]
 
 
 						#Send the tweets
-                        for outgoing_tweet in outgoing_tweets:
-                            api.update_status(status=outgoing_tweet,in_reply_to_status_id=status.id)
-                            print "Tweeting: %s" % outgoing_tweet
+						for outgoing_tweet in outgoing_tweets:
+							api.update_status(status=outgoing_tweet,in_reply_to_status_id=status.id)
+							print "Tweeting: %s" % outgoing_tweet
 
 						#Follow both users
-                        api.create_friendship(tweeter_screen_name)
-                        if username_to_tweet_like.lower() != my_username.lower():
-                            api.create_friendship(username_to_tweet_like)
-                else:
-                    if status.text.lower().startswith("@gotweetlike"):
-                        send_error_message(status.user.id)
+						api.create_friendship(tweeter_screen_name)
+						if username_to_tweet_like.lower() != my_username.lower():
+							api.create_friendship(username_to_tweet_like)
+				else:
+					if status.text.lower().startswith("@gotweetlike"):
+						send_error_message(status.user.id)
 
 
 
-    def on_error(self, status_code):
-        print "Error: %d" % status_code
-        if status_code == 420:
-            return False
+	def on_error(self, status_code):
+		print "Error: %d" % status_code
+		if status_code == 420:
+			return False
 # 			print "sleeping..."
 # 			time.sleep(60*15)
 # 			start_server()
-        else:
-            start_server()
+		else:
+			start_server()
 
 def start_server():
-    my_stream_listener = MyStreamListener()
-    my_stream = tweepy.Stream(auth=api.auth, listener=my_stream_listener)
-    print "Creating stream..."
-    try:
-        my_stream.filter(track=['@GoTweetLike'])
-    except AttributeError as e: # ignore error in Tweepy library (see here: https://github.com/tweepy/tweepy/issues/576)
-        print "Caught AttributeError, Restarting server (%s)" % e.message
-        start_server()
+	my_stream_listener = MyStreamListener()
+	my_stream = tweepy.Stream(auth=api.auth, listener=my_stream_listener)
+	print "Creating stream..."
+	try:
+		my_stream.filter(track=['@GoTweetLike'])
+	except AttributeError as e: # ignore error in Tweepy library (see here: https://github.com/tweepy/tweepy/issues/576)
+		print "Caught AttributeError, Restarting server (%s)" % e.message
+		start_server()
 
 start_server()
